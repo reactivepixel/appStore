e77a6cbba985f89bdfbd2f7501e2294dc337a4d8
checkkdahustle <checkkdahustle@fullsail.edu>
2016-04-14 16:47:31 -0400

    created a middleware to track rawRoute and add comments in code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,55 +1,64 @@
 module.exports = function(express) {
- var express = require('express');
- var histories = require('../models/histories.js');
- var db = require('../models/db.js');
- var router = express.Router();
+  var express = require('express');
+  var histories = require('../models/histories.js');
+  var db = require('../models/db.js');
+  var router = express.Router();
 
 
+// NOTES___________________________________________________
+  // made a variable point to histories.js in model folder
+  // middleware function is placed after the route.get
+  // ^^ because middleware won’t execute for GET requests
+  // will fire on every route that comes thru express
+/*
+    ********************************************************
+*/
 
-//middle ware route
-//will fire on every route that comes thru express
-//create a model for histories
+  // Standard Routes
+  router.get('/', function(req, res) {
+    res.status(200).json({
+      msg: 'Hello World',
+      healthy: true
+    });
+  });
 
-// a middleware function with no mount path. This code is executed for every request to the router
+  router.get('/status', function(req, res) {
+    res.status(200).json({
+      healthy: true
+    });
+  });
 
+/*
+  ********************************************************
+*/
 
- // Create
-router.use(function (req, res, next) {
-  var payload = req.body;
-  // add raw
-  payload.rawRoute = req.originalUrl;
- histories.create(req.body,function(err){
-     // Error Encountered
-     res.status(500).json(err);
-   },function(data) {
-     res.status(200).json(data);
-   });
- });
+  // Routes
+  router.use('/api/', require('./api/user')(express));
+  router.use('/api/', require('./api/app')(express));
+  router.use('/api/', require('./api/app_assets')(express));
+  router.use('/api/', require('./api/list')(express));
+  router.use('/api/', require('./api/listed_apps')(express));
+  // router.use('/api/', require('./api/app')(express));
 
-
-
- // Standard Routes
- router.get('/', function(req, res) {
-   res.status(200).json({
-     msg: 'Hello World',
-     healthy: true
-   });
- });
-
- router.get('/status', function(req, res) {
-   res.status(200).json({
-     healthy: true
-   });
- });
-
-
- // Routes
- router.use('/api/', require('./api/user')(express));
- router.use('/api/', require('./api/app')(express));
- router.use('/api/', require('./api/app_assets')(express));
- router.use('/api/', require('./api/list')(express));
- router.use('/api/', require('./api/listed_apps')(express));
-
-
- return router;
+/*
+  ********************************************************
+*/
+// MIDDLE-WARE
+  //used the existing module on index.js to add track user browsering in the url .
+  // this middleware function has no mount path.
+  // this code is executed for every request to the router.
+  // rawRoute of url is stored in database.
+  router.use(function (req, res, next) {
+    var payload = req.body;
+    // create full rawRoute of the url and store in db
+    payload.rawRoute = req.protocol + '://' + req.get('host') + req.originalUrl;
+    histories.create(payload,function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json(data);
+      next(); // end the request
+    });
+  });
+    return router;
 };
