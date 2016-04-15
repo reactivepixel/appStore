7ba743f687061ac2006c2dc450c164c1d42a82df
Chapman <Chapman@Apextion.com>
2016-04-15 19:47:27 -0400

    Fixing waterfall

--- seed/init.js
+++ seed/init.js
@@ -14,64 +14,67 @@ var apps = [];
 var users = [];
 var lists = [];
 
-function stdErr(err){
-  util.debug("Error",err)
+function stdErr(err) {
+    util.debug("Error", err);
 }
+
 function genSeedUser() {
-  var userSeed = {
-    dispName: faker.company.companyName(),
-    email: faker.internet.email(),
-    password: "unhashed",
-    phone: faker.phone.phoneNumber()
-  }
-  users.push(userSeed);
-  return userSeed;
+    var userSeed = {
+        dispName: faker.company.companyName(),
+        email: faker.internet.email(),
+        password: "unhashed",
+        phone: faker.phone.phoneNumber()
+    };
+    users.push(userSeed);
+    return userSeed;
 }
 
 function genSeedApp(input) {
-  var appSeed = {
-      title: faker.company.companyName(),
-      description: faker.company.bs(),
-      user_id: input.user_id || users[0].id,
-      readme: faker.internet.url(),
-      sourceLink: faker.internet.url(),
-      exeLink: faker.internet.url(),
-      iOSAppStoreLink: faker.internet.url(),
-      releaseDate: faker.date.future(),
-  }
-  apps.push(appSeed);
-  return appSeed;
+    var appSeed = {
+        title: faker.company.companyName(),
+        description: faker.company.bs(),
+        user_id: input.user_id || users[0].id,
+        readme: faker.internet.url(),
+        sourceLink: faker.internet.url(),
+        exeLink: faker.internet.url(),
+        iOSAppStoreLink: faker.internet.url(),
+        releaseDate: faker.date.future(),
+    };
+    apps.push(appSeed);
+    return appSeed;
 }
 
 function genSeedList(input) {
-  var listSeed = {
-      title: faker.company.companyName(),
-      user_id: input.user_id || users[0].id
-  }
-  lists.push(listSeed);
-  return listSeed;
+    var listSeed = {
+        title: faker.company.companyName(),
+        user_id: input.user_id || users[0].id
+    };
+    lists.push(listSeed);
+    return listSeed;
 }
 
-function done(){
-  console.log('all done');
+function done(data) {
+    console.log('all done', data);
 }
-async.waterfall([
+async.series([
 
-  function(callback) {
-    user.create(genSeedUser(), stdErr, (data) =>{
-      callback(null,data);
-    });
-  },
-  // function(createdUser, callback) {
-  //   app.create(genSeedApp({user_id: createdUser.id}), stdErr, (data) =>{
-  //     callback(createdUser,data);
-  //   });
-  // },
-  function(createdUser, callback) {
-    list.create(genSeedList({user_id: createdUser.id}), stdErr, (data) =>{
-      console.log('===========================',data);
-      callback(null,data);
-    });
-  },
-
-], done)
+    function(callback) {
+        user.create(genSeedUser(), stdErr, (data) => {
+            callback(null, data);
+        });
+    },
+    function(createdUser, callback) {
+        list.create(genSeedList({
+            user_id: createdUser.id
+        }), stdErr, (data) => {
+            callback(null, data);
+        });
+    },
+    function(createdUser, callback) {
+        app.create(genSeedApp({
+            user_id: createdUser.id
+        }), stdErr, (data) => {
+            callback(null, data);
+        });
+    },
+], done);
