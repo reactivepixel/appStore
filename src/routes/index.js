1d1de961153a0c5ed5c9e656d7ceda3b8e31b320
checkkdahustle <checkkdahustle@fullsail.edu>
2016-04-15 01:33:04 -0400

    uploading middleware for history tracking working file, but two error on unit testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,6 +31,8 @@ module.exports = function(express) {
 /*
   ********************************************************
 */
+// Route for JSDocs
+  router.use('/api', express.static(__dirname + '/out'));
 
   // Routes
   router.use('/api/', require('./api/user')(express));
