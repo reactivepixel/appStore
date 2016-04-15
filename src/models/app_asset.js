7fa607fbc5e62b74ff67ffdf4c6a908cb3830d44
Karl Rohr <kjrohr@fullsail.edu>
2016-04-15 19:38:34 -0400

    Added comments to src/models/app_asset.js

--- src/models/app_asset.js
+++ src/models/app_asset.js
@@ -1,12 +1,25 @@
 module.exports = function() {
-  var db = require('./db.js');
-  var sequelize = db.connection;
-  var util = require('../../lib/util');
+  var db = require('./db.js'); // Sets db to imported file db.js
+  var sequelize = db.connection; // Sets sequelize to db.connection
+  var util = require('../../lib/util'); // Sets util to imported util.js
 
-  function _create(payload, err, success) {
+
+  /*
+    New function _create accepts 3 parameters, payload, err, and success
+    Sets in scope variable cleanData to passed paramaeter payload.
+    todo's, research line 15 and explain
+  */
+  function _create(payload, err, success)
+  {
     var cleanData = payload;
     db.appAsset.create(cleanData).then(success).catch(err);
   }
+
+  /*
+   New function _update accepts 3 parameters, payload, err, and success
+   Sets in scope variable cleanData to passed parameter payload
+   todo's research lines 26 through 32 and explain
+  */
 
   function _update(payload, err, success) {
     var cleanData = payload;
@@ -19,6 +32,11 @@ module.exports = function() {
     }).catch(err);
   }
 
+  /*
+    New function _find accepts 3 parameters, payload, err, and success
+    Sets in scope variable cleanData to passed parameter Payload
+    todos' research line 41 and lines 43 - 51
+  */
   function _find(payload, err, success) {
     util.debug('App Asset Model _Find Payload', payload)
     var cleanData = payload;
@@ -33,6 +51,10 @@ module.exports = function() {
     }).then(success).catch(err);
   }
 
+  /*
+    New function _findAll accepts 2 parameters, error and success
+    todo's research lines 59 - 64
+  */
   function _findAll(err, success) {
     db.appAsset.findAll({
       include: [{
@@ -41,6 +63,12 @@ module.exports = function() {
       }]
     }).then(success).catch(err);
   }
+
+  /*
+    New function _destroy accepts 3 paramaters, palyoad, err, and success
+    Sets in scope variable cleanData to passed paramater paylaod
+    todo's research lines 75 - 80
+  */
 
   function _destroy(payload, err, success) {
     var cleanData = payload;
@@ -52,6 +80,11 @@ module.exports = function() {
     }).then(success).catch(err);
   }
 
+
+  /*
+    Returns an object with the functions?
+    todo's look more into this return
+  */
   return {
     create: _create,
     update: _update,
