885f781bd935d903771213c65368a29cca9e63d8
Lucas Cross <alucidisland@gmail.com>
2016-04-19 01:58:04 -0400

    Fixed jsdoc build in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,16 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var shell = require('gulp-shell');
 var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
 
 //Task to initiate JSdocs
-gulp.task('js-doc', shell.task(['jsdoc src/**/**/*']));
-
-// Task to move JSdoc 'out' folder to correct place with new name
-gulp.task('default', ['dev','js-doc'], function() {
-  gulp.src("./out/**/**/*")
-  .pipe(gulp.dest('build/JSdocs'));
-});
-
-// Task to remove 'out' folder
-gulp.task('default', function () {
-    return gulp.src('./out', {read: false})
-        .pipe(clean());
-});
+gulp.task('js-doc', shell.task(['jsdoc src -r -c ./conf.json -d ./build/jsdocs']));
 
 // Task to run server using gulp
-gulp.task('default', function () {
+gulp.task('server', function () {
   nodemon({
     script: './src/server.js'
   });
 });
+
+gulp.task('default', ['js-doc','server']);
