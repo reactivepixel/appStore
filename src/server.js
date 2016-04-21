d14c8b5fd4224f10080dd87ad504590590c01e61
Chapman <Chapman@Apextion.com>
2016-04-20 22:49:38 -0400

    removed dependency on dotenv file for fatal errors

--- src/server.js
+++ src/server.js
@@ -16,7 +16,10 @@ var body_parser = require('body-parser'); // Sets variable body_parser to body-p
 var app = express(); // Sets variable app to express function
 
 // Dot Env File Loader
-if (!process.env.PORT) dotenv = require('dotenv').load(); // If
+util.ifExists(process.env.PORT, function(){
+   dotenv = require('dotenv').load();
+});
+
 
 /**
  * @var {number} port
