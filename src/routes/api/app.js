86c4b9046d3ed22307f6cbb0a2b3284cefbe520d
Karl Rohr <karlrohr@annihilation.local>
2016-04-22 18:22:29 -0400

    Began annotating app.js

--- src/routes/api/app.js
+++ src/routes/api/app.js
@@ -1,9 +1,26 @@
 module.exports = function (express) { // Setting module.exports to a new function that excepts express as a paramater of express.
 
+<<<<<<< b71989d7ea7a43bd3a422bda5aabd415d410b334
+=======
 
-  var router = express.Router(); // Sets variable router to express.Router() function
-  var app = require('../../models/app'); // Sets app to imported app.js file
-  var util = require('../../../lib/util'); // Sets util to util.js file
+  /**
+   * @var {module} router
+   * This sets the express.Router function to the variable, router.
+  */
+  var router = express.Router();
+>>>>>>> Began annotating app.js
+
+  /**
+   * @var {file} app
+   * This sets the imported file app.js to the variable, app.
+  */
+  var app = require('../../models/app');
+
+  /**
+   * @var {file} util
+   * This sets the imported file util.js to the variable, util.
+  */
+  var util = require('../../../lib/util');
 
 
 
