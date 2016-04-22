44c48ad7c7fcf071d34327b70a5bf16512d56308
ptlergo <ptlergo@gmail.com>
2016-04-22 19:12:41 -0400

    remove test module, excess comments, added genre variable

--- src/models/recommend.js
+++ src/models/recommend.js
@@ -70,6 +70,33 @@ var a='patrick';
 
   //captured by express
   ger.initialize_namespace('movies')
+  var esm = new g.MemESM();//Event Store Manager
+  var ger = new g.GER(esm);//Good Enough Recommender----Added to package.json
+//
+//   //change test parameters
+//   //might use .put for postman
+var person='jeff';
+var person2='greg';
+var person3='loopy';
+//   var sports='sports';
+//   var thing='swimming';
+
+// //ger will produce a confidence amount which represents what to recommend to subject user.
+// //the higher the confidence the higher the thing/game will be recommended
+
+
+ger.initialize_namespace('movies')
+.then( function() {
+  return ger.events([
+    {
+      namespace: 'movies',
+      person: person,
+      action: 'likes',
+      thing: 'xmen',
+      expires_at: '2020-06-06'
+    }
+  ])
+  })
   .then( function() {
     return ger.events([
       {
@@ -102,3 +129,12 @@ var a='patrick';
 
   })
 };
+});
+sports_bucket_say = console.log("model and route connectted");
+sayit: "like this cause json";
+sayw=function(){
+  console.log('"like this cause json"');
+};
+
+return sports_bucket_say;
+}
