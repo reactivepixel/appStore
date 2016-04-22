05781bc8300f6104b2c49cc3fb2617c9cf14463d
Karl Rohr <karlrohr@annihilation.local>
2016-04-22 17:33:01 -0400

    Added annotations to listed_apps.js

--- src/routes/api/listed_apps.js
+++ src/routes/api/listed_apps.js
@@ -1,6 +1,24 @@
 module.exports = function (express) {
+  /**
+   * @var {module} router
+   * This sets the express.Router function to the variable, router.
+  */
   var router = express.Router();
+<<<<<<< 3a1b874af3a839672f4590e9b7cc62c6e1d90516
   var list = require('../../models/listed_apps');
+=======
+
+  /**
+   * @var {file} list
+   * This sets the imported file listed_apps.js to the variable, list.
+  */
+  var list = require('../../models/listed_apps.js');
+
+  /**
+   * @var {file} util
+   * This sets the imported file util.js to the variable, util.
+  */
+>>>>>>> Added annotations to listed_apps.js
   var util = require('../../../lib/util');
 
   // Create
