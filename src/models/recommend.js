22b551630379ed630e8aa4d585acc33f0efee56b
ptlergo <ptlergo@gmail.com>
2016-04-22 19:17:09 -0400

    remove test module, excess comments, added genre variable

--- src/models/recommend.js
+++ src/models/recommend.js
@@ -138,3 +138,55 @@ sayw=function(){
 
 return sports_bucket_say;
 }
+
+};
+module.exports = function(express) {
+  var g = require('ger');
+  var esm = new g.MemESM();//Event Store Manager
+  var ger = new g.GER(esm);//Good Enough Recommender----Added to package.json
+
+     module.exports.person = 5;
+  ger.initialize_namespace(express)
+  .then(function(){
+    return ger.events([
+      {
+        namespace: 'movies',
+        person: 'patrick',
+        action: 'likes',
+        thing: 'xmen',
+        expires_at: '2020-06-06'
+      },
+      {
+        namespace: 'movies',
+        person: 'joe',
+        action: 'likes',
+        thing: 'books',
+        expires_at: '2020-06-06'
+      },
+      {
+        namespace: 'movies',
+        person: 'john',
+        action: 'likes',
+        thing: 'xmen',
+        expires_at: '2020-06-06'
+      },
+      {
+        namespace: 'movies',
+        person: 'joe',
+        action: 'likes',
+        thing: 'circus',
+        expires_at: '2020-06-06'
+      }
+  ]);
+}).then(function(){
+    return ger.recommendations_for_thing('movies', 'xmen', {actions: {likes: 1}});
+  }).then(function(recommendations){
+    console.log(`Person: ${person}`+JSON.stringify(recommendations, null,2));
+  });
+
+
+
+
+
+
+};
