d65f51b9c0598815aca766565350442447ad9679
ptlergo <ptlergo@gmail.com>
2016-04-21 22:42:12 -0400

    commit alterations minor to recommend model. removed merge errors

--- src/models/recommend.js
+++ src/models/recommend.js
@@ -78,6 +78,17 @@ var a='patrick';
   var g = require('ger');
   var esm = new g.MemESM();//Event Store Manager
   var ger = new g.GER(esm);//Good Enough Recommender----Added to package.json
+<<<<<<< 2cd91473b86d78fc5790bae4bee0958ff98153d5
+=======
+//
+//   //change test parameters
+//   //might use .put for postman
+var person='jeff';
+var person2='greg';
+var person3='loopy';
+//   var sports='sports';
+//   var thing='swimming';
+>>>>>>> commit alterations minor to recommend model. removed merge errors
 
   //change test parameters
   //might use .put for postman
@@ -160,3 +171,31 @@ ger.initialize_namespace('movies')
 //4 types in api: person: string, thign: string, actionLstring, weight:integer
 //an event is a person performing an aciotn on a thing
 //set_action_weight(action, weight);
+<<<<<<< 2cd91473b86d78fc5790bae4bee0958ff98153d5
+=======
+
+// //ger will produce a confidence amount which represents what to recommend to subject user.
+// //the higher the confidence the higher the thing/game will be recommended
+
+
+ger.initialize_namespace('movies')
+.then( function() {
+  return ger.events([
+    {
+      namespace: 'movies',
+      person: person,
+      action: 'likes',
+      thing: 'xmen',
+      expires_at: '2020-06-06'
+    }
+  ])
+  })
+  .then( function() {
+  // What things might alice like?
+    return ger.recommendations_for_person('movies', person3, {actions: {likes: 1}})
+  })
+  .then( function(recommendations) {
+    // console.log(JSON.stringify(recommendations,null,2))
+  });
+};
+>>>>>>> commit alterations minor to recommend model. removed merge errors
