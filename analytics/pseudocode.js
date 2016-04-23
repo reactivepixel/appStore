50b735f20057d9b20b739ecad85b671c225d21a8
lindsayhampton <lrhampton@fullsail.edu>
2016-04-23 17:14:38 -0400

    Pseudocoded out Global Clock

--- analytics/pseudocode.js
+++ analytics/pseudocode.js
@@ -1,6 +1,30 @@
 /* 
 App Store Analytics Pseudocode
 
+~ Global Clock
+	- Uses moment.js
+		* Will detect the date and time
+		* Moment timezone can be used to detect each timezone for the dropdown menu
+	- Drop down menu for different time zones
+		* Moment.js timezone
+	- Styled with CSS / Bootstrap / choice framework
+	- Clock can be created two ways
+		* HTML5 Canvas
+		* CSS animation property
+			(https://cssanimation.rocks/clocks/)
 
+~ Toolbar
+
+~ App Updates Section
+
+~ Visitors Per Hour
+
+~ Most Popular Apps
+
+~ Daily Planner / To Do List
+
+~ Daily Sales
+
+~ Users
 
 */
