f41409038a969f7161132171ec5bdb7b5867dda9
Karl Rohr <karlrohr@annihilation.local>
2016-04-24 15:08:30 -0400

    Removed annotations from listed_apps.js as per pull request

--- src/routes/api/listed_apps.js
+++ src/routes/api/listed_apps.js
@@ -18,14 +18,7 @@ module.exports = function (express) {
   var util = require('../../../lib/util');
 
   // Create
-  /**
-   * @function put
-   * @param filePath
-   * ATTN: A route or filepath with unqiue data.
-   * @param function(req,res)
-   * This is a function that takes in req and res as parameters
-   * @desc ATTN: What does this do?
-  */
+
   router.put('/listed_app/:list_id/:app_id', function(req, res) {
     req.body.listId = req.params.list_id;
     req.body.appId = req.params.app_id;
@@ -39,15 +32,7 @@ module.exports = function (express) {
   });
 
 
-  // Delete One
-  /**
-   * @function delete
-   * @param filePath
-   * ATTN: A route or filepath with unqiue data.
-   * @param function(req,res)
-   * This is a function that takes in req and res as parameters
-   * @desc ATTN: What does this do?
-  */
+
   router.delete('/listed_app/:list_id/:app_id', function(req, res) {
     req.body.listId = req.params.list_id;
     req.body.appId = req.params.app_id;
