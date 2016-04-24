e500f64b88a5740733bf63e308dee615a39ef763
Karl Rohr <karlrohr@annihilation.local>
2016-04-24 16:32:54 -0400

    Moved annotations around for uniformity.

--- src/routes/api/voting.js
+++ src/routes/api/voting.js
@@ -15,37 +15,39 @@ module.exports = function(express) {
  * This sets the util voting to equal the path to the lib/util
 */
 
-  /** Read One. */
-  router.get('/voting/:vote_id', function(req, res) {
 /**
  * @function get
  * @description Gathers Data
- * @param req 
+ * @param req
  * Requests Data
  * @param res
- * Listens for response  
+ * Listens for response
 */
-    req.body.id = req.params.vote_id;
-/**
- * @property req.body
- * @description holds parameters that are sent up from the client as part of a POST request 
-*/
-/**
- * @property {attribute} id
- * Defines a unique identifier (ID) which must be unique in the whole document. Its purpose is to identify the element when linking
-*/
-/**
- * @property req.params
- * @description will return parameters in the matched route
-*/
+  /** Read One. */
+  router.get('/voting/:vote_id', function(req, res) {
 
-    voting.find(req.body, function(err) { 
-/**
- * @function find
- * @description Finds ID in DB using _find in models/voting.js
- * @param req.body
- * Requests the body information
-*/
+    /**
+     * @property req.body
+     * @description holds parameters that are sent up from the client as part of a POST request
+    */
+    /**
+     * @property {attribute} id
+     * Defines a unique identifier (ID) which must be unique in the whole document. Its purpose is to identify the element when linking
+    */
+    /**
+     * @property req.params
+     * @description will return parameters in the matched route
+    */
+    req.body.id = req.params.vote_id;
+
+    /**
+     * @function find
+     * @description Finds ID in DB using _find in models/voting.js
+     * @param req.body
+     * Requests the body information
+    */
+    voting.find(req.body, function(err) {
+
       /** ERROR Encountered. */
       res.status(500).json(err);
     }, function(data) {
@@ -53,24 +55,25 @@ module.exports = function(express) {
     });
   });
 
-
+  /**
+   * @function get
+   * @description Gathers Data
+   * @param req
+   * Requests Data
+   * @param res
+   * Listens for response
+  */
   /** Read All */
   router.get('/votes', function(req, res) {
-/**
- * @function get
- * @description Gathers Data 
- * @param req 
- * Requests Data
- * @param res
- * Listens for response 
-*/
+
+    /**
+     * @function findAll
+     * @description Finds all in DB using _findAll in model/voting.js
+     * @param err
+     * Error
+    */
     voting.findAll(function(err) {
-/**
- * @function findAll
- * @description Finds all in DB using _findAll in model/voting.js
- * @param err
- * Error
-*/
+
       /** ERROR Encountered. */
       res.status(500).json(err);
     }, function(data) {
@@ -78,24 +81,26 @@ module.exports = function(express) {
     });
   });
 
+  /**
+   * @function put
+   * @description Places Data
+   * @param req
+   * Requests Data
+   * @param res
+   * Listens for response
+  */
   /** Create */
   router.put('/app/:app_id/vote', function(req, res) {
-/**
- * @function put
- * @description Places Data
- * @param req 
- * Requests Data
- * @param res
- * Listens for response 
-*/
+
     req.body.app_id = req.params.app_id;
-    voting.create(req.body, function(err) { 
-/**
- * @function create
- * @description Runs _create in models to clean data and add to DB from app
- * @param req.body
- * Requests the body information
-*/
+    /**
+     * @function create
+     * @description Runs _create in models to clean data and add to DB from app
+     * @param req.body
+     * Requests the body information
+    */
+    voting.create(req.body, function(err) {
+
       /** ERROR Encountered. , try removing id and try again.*/
       delete req.body.id;
       voting.create(req.body, function(err) {
@@ -108,26 +113,29 @@ module.exports = function(express) {
     });
   });
 
+  /**
+   * @function put
+   * @description Places Data
+   * @param req
+   * Requests Data
+   * @param res
+   * Listens for response
+  */
   /** Update */
-  router.put('/app/:app_id/vote/:vote_id', function(req, res) { 
-/**
- * @function put
- * @description Places Data
- * @param req 
- * Requests Data
- * @param res
- * Listens for response 
-*/
-    req.body.app_id = req.params.app_id; 
-    req.body.id = req.params.vote_id; 
+  router.put('/app/:app_id/vote/:vote_id', function(req, res) {
+
+    req.body.app_id = req.params.app_id;
+    req.body.id = req.params.vote_id;
     util.debug('Voting Update Route Request Body', req.body);
-    voting.update(req.body, function(err) { 
-/**
- * @function update
- * @description varifies that app id and body.id match and runs _update in models
- * @param req.body
- * Requests the body information
-*/  
+
+    /**
+     * @function update
+     * @description varifies that app id and body.id match and runs _update in models
+     * @param req.body
+     * Requests the body information
+    */
+    voting.update(req.body, function(err) {
+
     /** ERROR Encountered. */
       res.status(500).json(err);
     }, function(data) {
@@ -137,14 +145,15 @@ module.exports = function(express) {
 
   /** Delete One */
   router.delete('/app/:id/vote/:vote_id', function(req, res) {
-    req.body.id = req.params.vote_id; 
+    req.body.id = req.params.vote_id;
+    /**
+     * @function destroy
+     * @description Deletes User ID using _destroy in models
+     * @param req.body
+     * Requests the body information
+    */
     voting.destroy(req.body, function(err) {
-/**
- * @function destroy
- * @description Deletes User ID using _destroy in models
- * @param req.body
- * Requests the body information
-*/
+
       // Error Encountered
       res.status(500).json(err);
     }, function(data) {
