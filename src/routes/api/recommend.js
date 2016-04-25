7b61927193db247929bb7a489e618124934954cc
seanedw1 <sedwards@fullsail.edu>
2016-04-25 18:50:25 -0400

    deleted unneseccary files

--- src/routes/api/recommend.js
+++ src/routes/api/recommend.js
@@ -1,15 +1,10 @@
+var buckets=require('../../models/recommend');
+
 /**
  * @var {module} buckets
  * @function require
  * @param filename
  * This needs the selected file.
 */
-
-var g = require('ger');
-var esm = new g.MemESM();
-var ger = new g.GER(esm);
-var buckets=require('../../models/recommend');
-
-var rec=require('../../models/rec');
 
 buckets();
