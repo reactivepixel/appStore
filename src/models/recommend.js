8b2eb537fe0ec41904f4fd6054b7c06d97752c3b
smvargas787 <smvargas@fullsail.edu>
2016-04-25 19:00:54 -0400

    adding helpful comments

--- src/models/recommend.js
+++ src/models/recommend.js
@@ -13,9 +13,9 @@ module.exports = function(express) {
 
 var genre='sports';
 
-  ger.initialize_namespace('sports')
+  ger.initialize_namespace('sports') //Creates tracking history for 'sports'
   .then(function(){
-    return ger.events([
+    return ger.events([ //Passing to a method through an array
       {
         namespace: 'sports',
         person: 'patrick',
@@ -45,10 +45,32 @@ var genre='sports';
         expires_at: '2020-06-06'
       }
   ]);
-}).then(function(){
+}).then(function(){ //What returns from the module
     return ger.recommendations_for_thing('sports', 'game3', {actions: {likes: 1}});
   }).then(function(recommendations){
     // console.log(`Person: ${person}`+JSON.stringify(recommendations, null,2));
   });
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Plan for the day 
+//how to store into json
+//how to push json into a function and an array
+//arrays and functions interact
+
+
