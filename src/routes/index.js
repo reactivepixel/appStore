064abf111a95a2c4630e904fa4db1cc870fb5a6e
Lucas Cross <alucidisland@gmail.com>
2016-04-25 20:03:10 -0400

    Stripe test intigration almost finished

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ module.exports = function(express) {
   var db = require('../models/db');
   var router = express.Router();
 
-// NOTES
+  // NOTES
   // made a variable point to history.js in model folder.
   // middleware function is placed after the route.get
   // ^^ because middleware won’t execute for GET requests
@@ -27,10 +27,11 @@ module.exports = function(express) {
   });
 
 
+  router.use('/public', express.static(__dirname + './../client/html'));
 
-
-// Routes
+  // Routes
   router.use('/jsdoc', express.static(__dirname + './../../build/jsdocs')); // JSdoc route
+
   router.use('/api/', require('./api/user')(express));
   router.use('/api/', require('./api/app')(express));
   router.use('/api/', require('./api/app_assets')(express));
@@ -44,18 +45,56 @@ module.exports = function(express) {
   // this middleware function has no mount path.
   // this code is executed for every request to the router.
   // rawRoute of url is stored in database.
-  router.use(function (req, res, next) {
-    var payload = req.body;
-    //rawRoute is db name
-    // create full rawRoute of the url and store in db
-    payload.rawRoute = path.join(req.protocol + '://' + req.get('host') + req.originalUrl);
-    history.create(payload,function(err){
-      // Error Encountered
-      res.status(500).json(err);
-    },function(data) {
-      res.status(200).json(data);
-      next(); // end the request
+  // router.use(function (req, res, next) {
+  //   var payload = req.body;
+  //   //rawRoute is db name
+  //   // create full rawRoute of the url and store in db
+  //   payload.rawRoute = path.join(req.protocol + '://' + req.get('host') + req.originalUrl);
+  //   history.create(payload,function(err){
+  //     // Error Encountered
+  //     res.status(500).json(err);
+  //   },function(data) {
+  //     res.status(200).json(data);
+  //     next(); // end the request
+  //   });
+  // });
+
+  router.get('/testfire', function (req,res){
+
+    // Set your secret key: remember to change this to your live secret key in production
+    // See your keys here https://dashboard.stripe.com/account/apikeys
+    var stripe = require("stripe")("sk_test_LUmKi0AVTawC8QDabKUXYjQZ");
+
+    // (Assuming you're using express - expressjs.com)
+    // Get the credit card details submitted by the form
+    var stripeToken =   stripe.tokens.create({
+      card: {
+        "number": '4242424242424242',
+        "exp_month": 12,
+        "exp_year": 2017,
+        "cvc": '123'
+      }
+    }, function(err, token) {
+      // asynchronously called
     });
+
+    var charge = stripe.charges.create({
+      amount: 1000, // amount in cents, again
+      currency: "usd",
+      source: stripeToken,
+      description: "Example charge"
+    }, function(err, charge) {
+      if (err && err.type === 'StripeCardError') {
+        // The card has been declined
+        res.json({success:false});
+      }else{
+        res.json({success:true});
+      }
+    });
+
+    // res.json({name:'Yooo'});
+
   });
+
   return router;
 };
