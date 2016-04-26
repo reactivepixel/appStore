b54dc4246eaf5455a4e7dd75220e38d3e63977ea
LevanceWam <lkwamley@fullsail.edu>
2016-04-25 21:42:03 -0400

    check

---
+++ src/routes/api/list.js
@@ -0,0 +1,61 @@
+module.exports = function (express) {
+  var router = express.Router();
+  var list = require('../../models/list.js');
+  var util = require('../../../lib/util');
+
+  // Read One
+  router.get('/list/:id', function(req, res) {
+    req.body.id = req.params.id;
+    list.find(req.body,function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json(data);
+    });
+  });
+
+  // Read All
+  router.get('/lists', function(req, res) {
+    list.findAll(function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json(data);
+    });
+  });
+
+  // Create
+  router.put('/list', function(req, res) {
+    util.debug('List Route Create Request', req.body);
+    list.create(req.body,function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json(data);
+    });
+  });
+
+  // Update
+  router.put('/list/:id', function(req, res) {
+    req.body.id = req.params.id;
+    list.update(req.body,function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json(data);
+    });
+  });
+
+  // Delete One
+  router.delete('/list/:id', function(req, res) {
+    req.body.id = req.params.id;
+    list.destroy(req.body,function(err){
+      // Error Encountered
+      res.status(500).json(err);
+    },function(data) {
+      res.status(200).json({success:data});
+    });
+  });
+
+  return router;
+};
