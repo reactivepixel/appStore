b6368227d1dfe3749ccdfea8267f67ee70121dd2
Chapman <Chapman@Apextion.com>
2016-05-13 19:19:16 -0400

    add AppAssets Mocha tests to es2015 rework

--- src/server.js
+++ src/server.js
@@ -6,16 +6,11 @@ const util = require('../lib/util');
 const port = process.env.PORT || 3000;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-// app.use('/', require('./routes')(express));
-
-app.post('/', (req, res) => {
-  console.log('------------', req.body);
-  res.json(req.body);
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use('/', require('./routes')(express));
 
 
 /**
